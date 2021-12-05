b451312e4a71b927b35a097e31c896c0e8ded08b
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-12-05 15:34:49 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <a href="">This is gonna be youtube disney clone</a>{" "}
+      </h1>
+    </div>
+  );
+}
+
+export default App;
