f7d762448b86a531e9478952677d1b7e02006015
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-12-05 16:44:51 +0100

    cta imgs added

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,51 @@ import React from "react";
 import styled from "styled-components";
 
 const Login = (props) => {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.png" alt="" />
+        </CTA>
+        <BgImage />
+      </Content>
+    </Container>
+  );
 };
 
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  background-image: url("/images/login-background.jpg");
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+`;
+const CTA = styled.div``;
+
+const CTALogoOne = styled.img``;
 export default Login;
