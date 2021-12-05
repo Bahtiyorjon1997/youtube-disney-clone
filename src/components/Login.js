ffba1bd8af403b6bbd14373b5bb5160758c7ebef
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-12-05 23:39:05 +0100

    front page ready

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,6 +9,12 @@ const Login = (props) => {
         <CTA>
           <CTALogoOne src={ctaLogo} alt="" />
           <SignUp>GET IT ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 26.12.2021, the price of Disney+
+            and the Disney bundle will increase by 1$
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
         </CTA>
         <BgImage />
       </Content>
@@ -81,6 +87,29 @@ const SignUp = styled.a`
   width: 100%;
   letter-spacing: 1.5px;
   font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  color: hsla(0, 0% 95.3%, 1);
+  font-size: 14px;
+  margin: 0 0 24px 0;
+  line-height: 1.5;
+  letter-spacing: 1.3px;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
 `;
 
 export default Login;
